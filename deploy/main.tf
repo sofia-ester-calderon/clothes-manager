99b93c51a19f42b85acb09d8af0b41f81397687b
Sofia Calderon <sofia.ester.calderon@gmail.com>
2020-07-09 14:37:43 +1000

    added terraform backend

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "clothes-manager-terraform-backend"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+
+
 provider "aws" {
   region = "us-east-1"
 }
