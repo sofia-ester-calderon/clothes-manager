9da3a09b96526fbef49af0121e535666da1a7e50
Sofia Calderon <sofia.ester.calderon@gmail.com>
2020-07-17 10:55:41 +1000

    added redux actions and reducer tests

---
+++ src/store/actions/optionsActions.test.js
@@ -0,0 +1,57 @@
+import thunk from "redux-thunk";
+import configureMockStore from "redux-mock-store";
+import optionsActions from "./optionsActions";
+import { EDIT_COLOR, LOAD_COLORS } from "./actionTypes";
+import colorApi from "../../api/colorsApi";
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe("given a color is edited", () => {
+  it("dispatches editColorSuccess action if api call was successful", () => {
+    colorApi.editColor = jest.fn().mockResolvedValue();
+
+    const color = { id: "def_col_1", name: "Red", hash: "#ff1100" };
+
+    const store = mockStore({});
+    return store.dispatch(optionsActions.editColor(color)).then(() => {
+      expect(colorApi.editColor).toHaveBeenCalledWith(color);
+      expect(store.getActions()).toEqual([{ type: EDIT_COLOR, color }]);
+    });
+  });
+
+  it("does not dispatch anything if api call was unsuccessful", () => {
+    colorApi.editColor = jest.fn().mockRejectedValue();
+
+    const color = { id: "def_col_1", name: "Red", hash: "#ff1100" };
+
+    const store = mockStore({});
+    return store.dispatch(optionsActions.editColor(color)).then(() => {
+      expect(colorApi.editColor).toHaveBeenCalledWith(color);
+      expect(store.getActions()).toEqual([]);
+    });
+  });
+});
+
+describe("given colors are loaded", () => {
+  it("dispatches setColors action if api call was successful", () => {
+    const colors = [{ id: "def_col_1", name: "Red", hash: "#ff1100" }];
+    colorApi.getColors = jest.fn().mockResolvedValue(colors);
+
+    const store = mockStore({});
+    return store.dispatch(optionsActions.initColors()).then(() => {
+      expect(colorApi.getColors).toHaveBeenCalled();
+      expect(store.getActions()).toEqual([{ type: LOAD_COLORS, colors }]);
+    });
+  });
+
+  it("does not dispatch anything if api call was unsuccessful", () => {
+    colorApi.getColors = jest.fn().mockRejectedValue();
+
+    const store = mockStore({});
+    return store.dispatch(optionsActions.initColors()).then(() => {
+      expect(colorApi.getColors).toHaveBeenCalled();
+      expect(store.getActions()).toEqual([]);
+    });
+  });
+});
---
+++ src/store/reducers/optionsReducer.test.js
@@ -0,0 +1,52 @@
+import { EDIT_COLOR, LOAD_COLORS } from "../actions/actionTypes";
+import optionsReducer from "./optionsReducer";
+
+const initialState = {
+  colors: [
+    { id: "1", name: "Red", hash: "#ff1100" },
+    { id: "2", name: "Green", hash: "#00a80b" },
+    { id: "3", name: "Blue", hash: "#0019bf" },
+  ],
+  categories: ["Tops", "Bottoms"],
+};
+
+describe("given an edit color action was dispatched", () => {
+  it("should return the state with the updated color and all the original values of the other options", () => {
+    const updatedColor = { id: "1", name: "newName", hash: "newHash" };
+    const action = {
+      type: EDIT_COLOR,
+      color: updatedColor,
+    };
+    const newState = optionsReducer(initialState, action);
+    expect(newState.colors).toHaveLength(3);
+    expect(newState.colors).toEqual(expect.arrayContaining([updatedColor]));
+    expect(newState.categories).toEqual(initialState.categories);
+  });
+
+  it("should return initial state if the color id does not exist", () => {
+    const updatedColor = { id: "wrong id", name: "newName", hash: "newHash" };
+    const action = {
+      type: EDIT_COLOR,
+      color: updatedColor,
+    };
+    const newState = optionsReducer(initialState, action);
+    expect(newState).toEqual(initialState);
+  });
+});
+
+describe("given an init color action was dispatched", () => {
+  it("should return the state with the new colors and all the original values of the other options", () => {
+    const colors = [
+      { id: "a", name: "colorA", hash: "hashA" },
+      { id: "b", name: "colorB", hash: "hashB" },
+    ];
+    const action = {
+      type: LOAD_COLORS,
+      colors,
+    };
+    const newState = optionsReducer(initialState, action);
+    expect(newState.colors).toHaveLength(2);
+    expect(newState.colors).toEqual(colors);
+    expect(newState.categories).toEqual(initialState.categories);
+  });
+});
