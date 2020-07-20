d0db8fcdcdd8cd2762779e7d1cbba356ad5c45f0
Sofia Calderon <sofia.ester.calderon@gmail.com>
2020-07-20 13:43:14 +1000

    removed json spaces

--- deploy/lambda.tf
+++ deploy/lambda.tf
@@ -17,18 +17,18 @@ resource "aws_iam_role" "lambda_exec" {
 
   assume_role_policy = <<EOF
  {
-   "Version": "2012-10-17",
-   "Statement": [
-     {
-       "Action": "sts:AssumeRole",
-       "Principal": {
-         "Service": "lambda.amazonaws.com"
-       },
-       "Effect": "Allow",
-       "Sid": ""
-     }
+   "Version":"2012-10-17",
+   "Statement":[
+      {
+         "Action":"sts:AssumeRole",
+         "Principal":{
+            "Service":"lambda.amazonaws.com"
+         },
+         "Effect":"Allow",
+         "Sid":""
+      }
    ]
- }
+}
  EOF
 
 }
